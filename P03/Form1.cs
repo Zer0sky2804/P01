089515d49f1784f58d3cf2520cb6a11c16e9e82d
oskeram.04 <oskeram.04@A222-14>
2022-02-23 09:12:46 +0100

    2

--- P03/Form1.cs
+++ P03/Form1.cs
@@ -33,5 +33,49 @@ namespace P03
             int nsn = x * y / u;
             MessageBox.Show(String.Format("NSD ({0}, {1}) = {2}\nNSN ({3}, {4}) = {5}", x, y, u, x, y, nsn));
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            double ap = 0;
+            int p = 0;
+            foreach (Control ctrl in panel1.Controls)
+            {
+                if (ctrl is TextBox)
+                {
+                    int a = rnd.Next(5, 21);
+                    (ctrl as TextBox).Text = a.ToString();
+                    if (a % 7 == 0)
+                    {
+                        ap += a;
+                        p++;
+                    }
+                }
+                else
+                
+                    if (ctrl is RadioButton)
+                    {          
+                    (ctrl as RadioButton).Enabled= !(ctrl as RadioButton).Enabled;
+                    }
+
+              else
+                   
+                        if (ctrl is Button)
+                        {
+
+                        }
+                   
+                
+            }
+            if (p != 0)
+            {
+                ap = ap / p;
+                label1.Text = "Aritmeticky prumer je " + ap;
+            }
+            else
+            {
+                label1.Text = "Aritmeticky prumer neexistuje.";
+            }
+        }
     }
 }
